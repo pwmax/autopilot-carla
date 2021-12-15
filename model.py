35a9eb1d0adfbeb68bb9da7e4e441dac492bae06
pwmax <hqwhq0@gmail.com>
2021-12-15 15:12:44 +0700

    fix model

--- model.py
+++ model.py
@@ -5,24 +5,23 @@ class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         self.block1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=24, kernel_size=5, stride=2),
+            nn.Conv2d(3, 24, 5, stride=2),
             nn.ReLU(),
-            nn.Conv2d(in_channels=24, out_channels=36, kernel_size=5, stride=2),
+            nn.Conv2d(24, 36, 5, stride=2),
             nn.ReLU(),
-            nn.Conv2d(in_channels=36, out_channels=48, kernel_size=5, stride=2),
+            nn.Conv2d(36, 48, 5, stride=2),
             nn.ReLU(),
-            nn.Conv2d(in_channels=48, out_channels=54, kernel_size=3, stride=1),
+            nn.Conv2d(48, 54, 3, stride=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=54, out_channels=64, kernel_size=3, stride=1),
+            nn.Conv2d(54, 64, 3, stride=1),
             nn.ReLU()
         )
         self.block2 = nn.Sequential(
-            nn.Linear(in_features=1152, out_features=100),
+            nn.Linear(1152, 100),
             nn.ReLU(),
-            nn.Linear(in_features=100, out_features=10),
+            nn.Linear(100, 10),
             nn.ReLU(),
-            nn.Linear(in_features=10, out_features=2),
-            nn.Tanh()
+            nn.Linear(10, 2),
         )
     
     def forward(self, x):
